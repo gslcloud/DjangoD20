80b52154418fc454c0efe36d8c0c5050b5193086
gslcloud <154714796+gslcloud@users.noreply.github.com>
2024-01-19 18:41:52 -0600

    init

---
+++ .src/authentication/__init__.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+
+# Custom authentication backend
+class CustomAuthenticationBackend:
+
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            pass
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
