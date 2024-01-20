32ea7369f378b8acb2571b3dbf17aab82f6571b6
gslcloud <154714796+gslcloud@users.noreply.github.com>
2024-01-19 18:42:14 -0600

    init

---
+++ .src/visualization/generate_charts.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def generate_chart(data):
+    # Code for generating the chart using Matplotlib
+    plt.plot(data)
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Chart')
+    plt.show()
